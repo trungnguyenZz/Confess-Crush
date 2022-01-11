0098c11374bd00756754af64d2f5727585091821
trungnguyenZz <97521930+trungnguyenZz@users.noreply.github.com>
2022-01-11 19:06:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Em ơi mình đã iu nhau được 2 năm rồi đấy🥰🥰🥰 ",
-    introTitle: 'Gửi emm',
+    introTitle: 'Gửi Thà Nghimmm',
     introDesc: `Trái đất vốn lạ thường
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim e lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy. Đừng giận anh nữa nha ',
+    title: 'Em ơi mình đã iu nhau được 2 năm rùi đấy. Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy 🥰',
+    desc: 'Anh biết anh hay làm em giận. Cho anh xin lũi nhaa  😘😘',
     btnYes: 'Oki anh <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    question: 'Đừng giận anh nữa nhaaaaa <3',
+    btnReply: 'Gửi anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Chiều nay 4h, anh qua đón đi chơi nha.',
